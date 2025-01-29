0bbe9f175cebcc29115b10d31ffc173b19a0605b
raunaksaigal <saigalraunak@gmail.com>
2025-01-29 22:23:21 +0530

    initial node connections added

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class test_api(APIView):
+
+    def get(self, request):
+        return Response("hi", status=status.HTTP_200_OK)
+
 
 # Create your views here.
